b95f09fdc290bcd728a2f0d4a31f0513f4b11dcd
Patricio Tomas Rinaldi <patricio.rinaldi@protonmail.com>
2025-08-14 20:35:44 +0200

    Agregado punto 6

---
+++ 1- Introducción a Java/Mensaje.java
@@ -0,0 +1,6 @@
+public class Mensaje {
+    
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Pérez \nEdad: 30 años \nDirección: \"Calle Falsa 123\"");
+    }
+}
