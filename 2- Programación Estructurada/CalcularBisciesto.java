70184daaa846973a8c888f00606e6b976f04eb91
Patricio Tomas Rinaldi <patricio.rinaldi@protonmail.com>
2025-08-28 18:46:23 +0200

    Agregado TP2 Ejercicio 1

---
+++ 2- Programación Estructurada/CalcularBisciesto.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class CalcularBisciesto {
+    
+    private int anio;
+    private Scanner scanner;
+
+    public CalcularBisciesto(){
+        this.anio = 0;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void solicitarAnio() {
+        System.out.print("Ingrese un año: ");
+        anio = scanner.nextInt();
+    }
+    
+    public void imprimir(){
+        if (esBisciesto(anio)) {
+            System.out.println("El año " + anio + " es bisciesto.");
+        } else {
+            System.out.println("El año " + anio + " no es bisciesto.");
+        }
+    }
+
+    public boolean esBisciesto(int anio) {
+        if (anio % 400 == 0 || (anio % 4 == 0 && anio % 100 != 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        CalcularBisciesto calc = new CalcularBisciesto();
+        calc.solicitarAnio();
+        calc.imprimir();
+    }
+}
